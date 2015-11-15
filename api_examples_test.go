7b8e3b75be1202bca6733ec583b46cad00fbb47a
Yusuke Komatsu <tienlen042@gmail.com>
2015-11-15 14:20:53 +0900

    add examples

---
+++ api_examples_test.go
@@ -0,0 +1,28 @@
+package dmm
+
+import "fmt"
+
+func ExampleApi() {
+    api := New("foobarbazbuzz", "dummy-990")
+    api.SetSite(SITE_ALLAGES)
+    api.SetService("mono")
+    api.SetFloor("dvd")
+    api.SetSort("date")
+    api.SetLength(1)
+
+    result, err := api.Execute()
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Println(result)
+    }
+}
+
+func ExampleApiOneLiner() {
+    rst, err := New("foobarbazbuzz", "dummy-999").SetSite(SITE_ADULT).SetLength(1).Execute()
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Println(rst)
+    }
+}
